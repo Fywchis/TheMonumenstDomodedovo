d0ff92d0870994f9f346a960e054abcab7cbf0c9
Fywchis <levonchello08@gmail.com>
2024-11-01 22:41:54 +0300

    Change database monument class, add library

--- Monument.py
+++ Monument.py
@@ -1,8 +1,9 @@
 
 class TheMonument:
-    def __init__(self):
-        address = ''
-        description = ''
+    def __init__(self, deg_x, deg_y, description):
+        self.deg_x = deg_x
+        self.deg_y = deg_y
+        self.description = description
 
     def clicked(self):
         pass
