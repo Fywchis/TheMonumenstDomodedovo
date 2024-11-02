cf913dd88e622ae5f8841b00b2b88f41f13f850e
Fywchis <levonchello08@gmail.com>
2024-11-02 19:33:53 +0300

    Add name for 1st marker and start new function

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 import tkintermapview as tkm
 import Monument as Mn
 
+def left_clicked():
+    pass
+
 window = Tk()
-window.title('TheMonuments of Domodedovo')
+window.title('Достопримечательности Домодедово')
 window.geometry('960x600')
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -24,8 +27,10 @@ map_widget.set_position(55.4407981, 37.7516731)
 
 # map_widget.set_zoom(14)
 map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=19)
-q = Mn.TheMonument(deg_x= 55.440687,deg_y=37.766823,description="")
-map_widget.set_marker(q.deg_x,q.deg_y)
+Obelisk = Mn.TheMonument(deg_x=55.440687, deg_y=37.766823, name="Обелиск славы", address="", description="")
+# adr = tkm.convert_coordinates_to_address(55.440687, 37.766823)
+# print(adr.street, adr.housenumber, adr.postal, adr.city, adr.state, adr.country, adr.latlng)
+map_widget.set_marker(Obelisk.deg_x, Obelisk.deg_y, Obelisk.name, text_color="white")
 
 
 window.mainloop()
