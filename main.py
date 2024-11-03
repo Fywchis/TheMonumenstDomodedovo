dd44245b2109bf54b96e8e7dd880ad717483deaf
Fywchis <levonchello08@gmail.com>
2024-11-03 15:37:15 +0300

    Add min_zoom

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ import os
 import tkintermapview as tkm
 import Monument as Mn
 
-def left_clicked():
-    pass
+MIN_ZOOM_LEVEL = 16
+
+
+def enforce_min_zoom():
+    print(map_widget.zoom)
+    current_zoom = map_widget.zoom
+    if current_zoom < MIN_ZOOM_LEVEL:
+        map_widget.set_zoom(MIN_ZOOM_LEVEL)
+
+    window.after(10, enforce_min_zoom)
+
+
+def left_clicked(marker):
+    print(marker.position)
+
 
 window = Tk()
 window.title('Достопримечательности Домодедово')
@@ -16,21 +29,24 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(script_directory, "offline_tiles.db")
 
 
-map_widget = tkm.TkinterMapView(window, width=960, height=600, corner_radius=0, use_database_only=True,
-                                database_path=str(database_path),
+map_widget = tkm.TkinterMapView(window, width=960, height=600, corner_radius=0,
+                                use_database_only=True, database_path=str(database_path),
                                 max_zoom=20)
 map_widget.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-
-map_widget.place(x=0, y=0)
 map_widget.set_position(55.4407981, 37.7516731)
+map_widget.set_zoom(MIN_ZOOM_LEVEL)
+map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga",
+                           max_zoom=19)
 
-# map_widget.set_zoom(14)
-map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=19)
-Obelisk = Mn.TheMonument(deg_x=55.440687, deg_y=37.766823, name="Обелиск славы", address="", description="")
+Obelisk = Mn.TheMonument(deg_x=55.440687, deg_y=37.766823, name="Обелиск славы",
+                         address="", description="")
 # adr = tkm.convert_coordinates_to_address(55.440687, 37.766823)
-# print(adr.street, adr.housenumber, adr.postal, adr.city, adr.state, adr.country, adr.latlng)
-map_widget.set_marker(Obelisk.deg_x, Obelisk.deg_y, Obelisk.name, text_color="white")
+# print(adr.street, adr.housenumber, adr.postal, adr.city,
+#       adr.state, adr.country, adr.latlng)
+obelisk_marker = map_widget.set_marker(Obelisk.deg_x, Obelisk.deg_y,
+                                       Obelisk.name, text_color="white")
+obelisk_marker.click(left_clicked)
 
+enforce_min_zoom()
 
 window.mainloop()
